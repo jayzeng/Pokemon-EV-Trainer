0dd4e76826839a8cd5d0061f9921205d98304a80
Jeffrey L <business.jeffrey.li@gmail.com>
2023-05-16 15:13:02 -0700

    project MVP stage

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Display from './components/Display';
+import Nav from './components/Nav';
+import CreatePokemon from './components/CreatePokemon';
+import UpdatePokemon from './components/UpdatePokemon';
+import PokemonDetail from './components/PokemonDetail';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Nav/>
+        <Routes>
+          <Route element={<Display/>} path="/myPokemon" default />
+          <Route element={<CreatePokemon/>} path="/Pokemon/new" />
+          <Route element={<PokemonDetail/>} path="/Pokemon/:id" />
+          <Route element={<UpdatePokemon/>} path="/Pokemon/:id/edit" />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
